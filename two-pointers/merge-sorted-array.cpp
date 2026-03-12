6c046e3e5a37508822a6e39871dc24b28775f8fa
Monica Jiang <19975057495@163.com>
2026-03-12 15:36:57 +0800

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/merge-sorted-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int end_1 = m - 1;
+        int end_2 = n - 1;
+        int end_merge = m + n - 1;
+        while (end_1 >= 0 && end_2 >= 0) {
+            if (nums1[end_1] >= nums2[end_2]) {
+                nums1[end_merge] = nums1[end_1];
+                end_merge--;
+                end_1--;
+            }
+            else {
+                nums1[end_merge] = nums2[end_2];
+                end_merge--;
+                end_2--;
+            }
+        }
+        if (end_2 != 0) {
+            while (end_2 >= 0) {
+                nums1[end_merge] = nums2[end_2];
+                end_2--;
+            }
+        }
+    }
+};
