aee4c7e6dfceb1983768ecd1ebdd5af1967a8b53
Monica Jiang <19975057495@163.com>
2026-03-15 21:37:39 +0800

    LC-linked-list-cycle [Runtime: 8 ms; Memory: 11.8 MB]

--- hash-table/linked-list-cycle.cpp
+++ hash-table/linked-list-cycle.cpp
@@ -8,7 +8,22 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr) 
+            return false; // 空链表无环
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next; // 慢指针走一步
+            fast = fast->next->next;  // 快指针走两步
+
+            if (slow == fast) { // 相遇则存在环
+                return true;
+            }
+        }
+
+        return false; // 快指针到达末尾，无环
     }
 };
