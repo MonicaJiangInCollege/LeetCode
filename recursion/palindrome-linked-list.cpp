8cbcd2bf0e2da1abd45c2dcd7d0114935eaed459
Monica Jiang <19975057495@163.com>
2026-03-26 10:13:42 +0800

    LC-palindrome-linked-list [Runtime: 0 ms; Memory: 132.1 MB]

---
+++ recursion/palindrome-linked-list.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> vals;
+        ListNode* cur = head;
+        while (cur) {
+            vals.push_back(cur->val);
+            cur = cur->next;
+        }
+        int left = 0, right = vals.size() - 1;
+        while (left < right) {
+            if (vals[left] != vals[right]) 
+            return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
