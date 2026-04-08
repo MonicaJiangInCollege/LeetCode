2a9de53bf37dc82755423c7aff2732c5e18b5aea
Monica Jiang <19975057495@163.com>
2026-04-08 16:25:56 +0800

    LC-minimum-absolute-difference-in-bst [Runtime: 0 ms; Memory: 25.2 MB]

---
+++ breadth-first-search/minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        int minDiff = INT_MAX;
+        int prev = -1;  // 保存中序遍历中前一个节点的值
+        inorder(root, prev, minDiff);
+        return minDiff;
+    }
+
+private:
+    void inorder(TreeNode* node, int& prev, int& minDiff) {
+        if (!node) return;
+        inorder(node->left, prev, minDiff);
+        if (prev != -1) {
+            minDiff = min(minDiff, node->val - prev);
+        }
+        prev = node->val;
+        inorder(node->right, prev, minDiff);
+    }
+};
