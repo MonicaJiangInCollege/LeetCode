38ff31661f6272eef2b2e1c3903eec9c4aef1fcd
Monica Jiang <19975057495@163.com>
2026-04-24 16:29:11 +0800

    LC-merge-two-binary-trees [Runtime: 0 ms; Memory: 33.6 MB]

---
+++ tree/merge-two-binary-trees.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        // If one of the trees is null, return the other tree
+        if (!root1) return root2;
+        if (!root2) return root1;
+        // Both nodes exist: sum their values
+        TreeNode* merged = new TreeNode(root1->val + root2->val);
+        // Recursively merge left and right children
+        merged->left = mergeTrees(root1->left, root2->left);
+        merged->right = mergeTrees(root1->right, root2->right);
+        return merged;
+    }
+};
