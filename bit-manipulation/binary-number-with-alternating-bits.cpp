1cecd0fbc0a965e735a9b1501bca4dbb743cf4b8
Monica Jiang <19975057495@163.com>
2026-04-28 22:18:42 +0800

    LC-binary-number-with-alternating-bits [Runtime: 2 ms; Memory: 7.8 MB]

--- bit-manipulation/binary-number-with-alternating-bits.cpp
+++ bit-manipulation/binary-number-with-alternating-bits.cpp
@@ -1,24 +1,7 @@
 class Solution {
 public:
-    int calPoints(vector<string>& operations) {
-        vector<int> scores;
-        for (const string& op : operations) {
-            if (op == "+") {
-                int sum = scores[scores.size() - 1] + scores[scores.size() - 2];
-                scores.push_back(sum);
-            } else if (op == "D") {
-                int doublePrev = scores.back() * 2;
-                scores.push_back(doublePrev);
-            } else if (op == "C") {
-                scores.pop_back();
-            } else {
-                scores.push_back(stoi(op));
-            }
-        }
-        int total = 0;
-        for (int score : scores) {
-            total += score;
-        }
-        return total;
+    bool hasAlternatingBits(int n) {
+        long long x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
     }
 };
