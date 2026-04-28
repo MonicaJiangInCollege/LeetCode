b8fa6f3fdbe5aa4bf8b43c9aec1bd405e3889017
Monica Jiang <19975057495@163.com>
2026-04-28 22:02:33 +0800

    LC-image-smoother [Runtime: 11 ms; Memory: 26.2 MB]

---
+++ array/image-smoother.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size(), n = img[0].size();
+        vector<vector<int>> res(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int sum = 0, count = 0;
+                for (int dx = -1; dx <= 1; ++dx) {
+                    for (int dy = -1; dy <= 1; ++dy) {
+                        int ni = i + dx, nj = j + dy;
+                        if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                            sum += img[ni][nj];
+                            count++;
+                        }
+                    }
+                }
+                res[i][j] = sum / count; // integer division floors automatically
+            }
+        }
+        return res;
+    }
+};
