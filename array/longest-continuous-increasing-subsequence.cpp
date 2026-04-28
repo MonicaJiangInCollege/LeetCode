fb525621947babc11db99a54eaa884a82dd37a11
Monica Jiang <19975057495@163.com>
2026-04-28 22:13:38 +0800

    LC-longest-continuous-increasing-subsequence [Runtime: 0 ms; Memory: 14.8 MB]

--- array/longest-continuous-increasing-subsequence.cpp
+++ array/longest-continuous-increasing-subsequence.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
-        
+        if (nums.empty()) return 0;
+        int maxLen = 1, curLen = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1]) {
+                curLen++;
+                maxLen = max(maxLen, curLen);
+            } else {
+                curLen = 1;
+            }
+        }
+        return maxLen;
     }
 };
